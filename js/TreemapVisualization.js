48586ab99c019b74a507a87eaaaeaa2c8f309767
Josh Cothran <josh.cothran@gmail.com>
2013-08-24 01:32:58 -0400

    Add files to GIT repo

---
+++ js/TreemapVisualization.js
@@ -0,0 +1,310 @@
+function TreemapVisualization() {
+	var tt_service_title = $("#tt_service_title")
+	var chart = d3.select("#chart");
+	var rows;
+	var row_labels = new Object();
+	var cols = new Object();
+	
+	var dimension_test = $("#dimension_test");
+	var row_label_height;
+	var payer_label_height;
+	var payer_label_widths = new Object();
+
+	this.render = function() {		
+		// Measure text dimensions
+		row_label_height = getTextHeight("Test", "row_label_text");
+		payer_label_height = getTextHeight("Test", "payer_cell_text");
+		_.each(payers_elements, function(payer_id) {
+			payer_label_widths[payer_id] = getTextWidth(payers_legend[payer_id]["title"], "payer_cell_text");
+		});
+		
+		jq_year_label.css("width", CHART_WIDTH - ROW_LABEL_WIDTH + "px");
+		jq_year_label.css("height", CHART_HEIGHT + "px");
+		jq_year_label.css("left", ROW_LABEL_WIDTH + CELL_BORDER_LR + "px");
+		jq_year_label.css("top", CELL_BORDER_TB + "px");
+		jq_year_label.css("line-height", CHART_HEIGHT + "px");
+		
+		// Init rows
+		var row_spending_tally = 0;
+		rows = chart.selectAll("div.tree_row")
+				.data(services_elements)
+			.enter().append("div")
+				.attr("id", function(d) {return d;})
+				.classed("tree_row", true)
+				.style("top", function(d) {
+					t = rowTop(row_spending_tally);
+					row_spending_tally += getSpending(d, TOTAL_PAYERS);
+					var h = rowHeight(d);
+					if (h < 4) return (t - 4) + "px";
+					else return t + "px";
+				})
+				.style("height", function(d) {
+					var h = rowHeight(d);
+					if (h < 4) return "4px";
+					else return h + "px";	
+				})
+				
+		// Init rows
+		var row_rollovers_spending_tally = 0;
+		row_rollovers = chart.selectAll("div.tree_row_rollover")
+				.data(services_elements)
+			.enter().append("div")
+				.attr("id", function(d) {return d + "_rollover";})
+				.classed("tree_row_rollover", true)
+				.style("width", CHART_WIDTH - ROW_LABEL_WIDTH + "px")
+				.style("left", ROW_LABEL_WIDTH + "px")
+				.style("border-width", "0px " + CELL_BORDER_LR + "px " + CELL_BORDER_TB + "px " + CELL_BORDER_LR + "px")
+				.attr("service", function(d) {return d;})
+				.style("top", function(d) {
+					t = rowTop(row_rollovers_spending_tally);
+					row_rollovers_spending_tally += getSpending(d, TOTAL_PAYERS);
+					var h = rowHeight(d);
+					if (h < 4) return (t - 4) + "px";
+					else return t + "px";
+				})
+				.style("height", function(d) {
+					var h = rowHeight(d);
+					if (h < 4) return "4px";
+					else return h + "px";	
+				})
+								
+		// Init columns
+		_.each(services_elements, function(service_id) {			
+			var row = d3.select("#" + service_id);
+			var col_spending_tally = 0;
+
+			// Row Labels
+			row_labels[service_id] = row.append("div")
+					.classed("row_label", true)
+					.style("width", ROW_LABEL_WIDTH - ROW_LABEL_PADDING_RT + "px")
+					.style("padding-right", ROW_LABEL_PADDING_RT + "px")
+					.html(function() {
+						// if (row_label_height < rowHeight(service_id)) {
+							var spendingAmountLabel = getServiceSpendingAmountLabel(service_id, true);
+							return services_legend[service_id]["title"] + spendingAmountLabel;
+						// }
+						// return ""; 
+					});
+			
+			// Columns		
+			cols[service_id] = row.selectAll("div.payer_cell")
+					.data(payers_elements)
+				.enter().append("div")
+					.property("className", function(d) {return d;})
+					.classed("cell", true)
+					.classed("payer_cell", true)
+					.classed("payer_cell_text", true)
+					.classed(service_id, true)
+					.style("border-width", CELL_BORDER_TB + "px " + CELL_BORDER_LR + "px " + CELL_BORDER_TB + "px " + CELL_BORDER_LR + "px")
+					.style("left", function(d) {
+						var l = colLeft(service_id, col_spending_tally) + ROW_LABEL_WIDTH;
+						col_spending_tally += getSpending(service_id, d);
+						return l + "px";
+					})
+					.style("width", function(d) { return colWidth(service_id, d)  + "px";	});
+		});
+		
+		initRollovers();		
+	}
+	
+	
+	this.update = function() {		
+		var row_spending_tally = 0;
+		var row_rollovers_spending_tally = 0;
+		if (animate) {
+			rows.transition()
+				.duration(TRANSITION_DURATION)
+				.style("top", function(d) {
+					t = rowTop(row_spending_tally);
+					row_spending_tally += getSpending(d, TOTAL_PAYERS);
+					var h = rowHeight(d);
+					if (h < 4) return (t - 4) + "px";
+					else return t + "px";
+				})
+				.style("height", function(d) {
+					var h = rowHeight(d);
+					if (h < 4) return "4px";
+					else return h + "px";	
+				})
+			row_rollovers.transition()
+				.duration(TRANSITION_DURATION)
+				.style("top", function(d) {
+					t = rowTop(row_rollovers_spending_tally);
+					row_rollovers_spending_tally += getSpending(d, TOTAL_PAYERS);
+					var h = rowHeight(d);
+					if (h < 4) return (t - 4) + "px";
+					else return t + "px";
+				})
+				.style("height", function(d) {
+					var h = rowHeight(d);
+					if (h < 4) return "4px";
+					else return h + "px";	
+				});
+		}
+		else {			
+			rows
+				.style("top", function(d) {
+					t = rowTop(row_spending_tally);
+					row_spending_tally += getSpending(d, TOTAL_PAYERS);
+					var h = rowHeight(d);
+					if (h < 4) return (t - 4) + "px";
+					else return t + "px";
+				})
+				.style("height", function(d) {
+					var h = rowHeight(d);
+					if (h < 4) return "4px";
+					else return h + "px";	
+				});
+			row_rollovers
+				.style("top", function(d) {
+					t = rowTop(row_rollovers_spending_tally);
+					row_rollovers_spending_tally += getSpending(d, TOTAL_PAYERS);
+					var h = rowHeight(d);
+					if (h < 4) return (t - 4) + "px";
+					else return t + "px";
+				})
+				.style("height", function(d) {
+					var h = rowHeight(d);
+					if (h < 4) return "4px";
+					else return h + "px";	
+				});
+		}
+	
+		_.each(services_elements, function(service_id) {
+			var col_spending_tally = 0;
+			var row_label = row_labels[service_id];
+			var col = cols[service_id];
+			
+			// no transition for html
+			row_label
+				.html(function() {
+					// if (row_label_height < rowHeight(service_id)) {
+						var spendingAmountLabel = getServiceSpendingAmountLabel(service_id, true);
+						return services_legend[service_id]["title"] + spendingAmountLabel;
+					// }
+					// return ""; 
+				});
+			
+			if(animate) {
+				col.transition()
+					.duration(TRANSITION_DURATION)
+					.style("left", function(d) {
+						l = colLeft(service_id, col_spending_tally) + ROW_LABEL_WIDTH;
+						col_spending_tally += getSpending(service_id, d);
+						return l + "px";
+					})
+					.style("width", function(d) { return colWidth(service_id, d)  + "px"; })
+					.style("line-height", function() {return rowHeight(service_id) + "px";})	
+			} else {
+				col
+					.style("left", function(d) {
+						l = colLeft(service_id, col_spending_tally) + ROW_LABEL_WIDTH;
+						col_spending_tally += getSpending(service_id, d);
+						return l + "px";
+					})
+					.style("width", function(d) { return colWidth(service_id, d)  + "px"; })
+			}
+		});		
+	}
+	
+	var getWidth = function() {return CHART_WIDTH - ROW_LABEL_WIDTH; };
+	var getHeight = function() {return CHART_HEIGHT };
+	
+	var inspect = function(obj) {
+		console.log(obj);
+	}	
+	
+	var rowHeight = function(service) {
+		var val = getSpending(service, TOTAL_PAYERS)
+		var height = val / getSpending(TOTAL_SERVICES, TOTAL_PAYERS) * getHeight();
+		return Math.round(height);
+	}
+	
+	var colWidth = function(service, payer) {
+		var val = getSpending(service, payer);
+		var width = val / getSpending(service, TOTAL_PAYERS) * getWidth();
+		return Math.round(width);
+	}
+	
+	var rowTop = function(val) {
+		var top = val / getSpending(TOTAL_SERVICES, TOTAL_PAYERS) * getHeight();
+		return Math.round(top);
+	}
+	
+	var colLeft = function(service, val) {
+		var left = val / getSpending(service, TOTAL_PAYERS) * getWidth();
+		return Math.round(left);
+	}
+	
+	var getTextWidth = function(text, text_class) {
+		dimension_test.toggleClass(text_class);
+		dimension_test.html(text);
+		var w = dimension_test.width();
+		dimension_test.html("");
+		dimension_test.toggleClass(text_class);
+		return w;
+	}
+	
+	var getTextHeight = function(text, text_class) {
+		dimension_test.toggleClass(text_class);
+		dimension_test.html(text);
+		var h = dimension_test.height();
+		dimension_test.html("");
+		dimension_test.toggleClass(text_class);
+		return h;
+	}
+	
+	var getPayerSpendingAmountLabel = function(s, p) {
+		var val = getSpending(s, p);
+		var percent = formatPercent(val / getSpending(s, TOTAL_PAYERS));
+		val = formatSpending(val);
+		return "&nbsp;| $" + val + "B (" + percent + ")";		
+	}
+	
+	var getServiceSpendingAmountLabel = function(s, show_percent) {
+		var val = getSpending(s, TOTAL_PAYERS);
+		var percent = formatPercent(val / getSpending(TOTAL_SERVICES, TOTAL_PAYERS));
+		val = formatSpending(val);
+		return show_percent ? 
+				"&nbsp;| $" + val + "B (" + percent + ")" :
+				"&nbsp;| $" + val + "B";	
+	}	
+	
+	var initRollovers = function() {
+		$(".tree_row_rollover").mouseenter(function() {
+			if (!year_playing) {
+				var service = $(this).attr("service");
+				var service_row = $("#" + service);
+				var service_top = parseInt(service_row.css("top").replace("px", ""));
+				var service_height = parseInt(service_row.css("height").replace("px", ""));
+				
+				var top = service_top + (service_height / 2) - (TT_HEIGHT / 2)
+				top = Math.round(top);
+				if ((top + TT_HEIGHT) > CHART_HEIGHT) top = CHART_HEIGHT - TT_HEIGHT + TT_BORDER_WIDTH;
+				else if (top < 0) top = TT_BORDER_WIDTH;
+				jq_tooltip.css("top", top + "px");
+				
+				var arrow_top = service_top + (service_height / 2) - (TT_ARROW_HEIGHT / 2);
+				arrow_top = Math.round(arrow_top);
+				jq_tooltip_arrow.css("top", arrow_top + "px");
+				
+				tt_service_title.html(services_legend[service].title);
+				
+				$("#tt_spending_total").html(getServiceSpendingAmountLabel(service, false));			
+				_.each(payers_elements, function(payer) {
+					$("#tt_spending_" + payer).html(getPayerSpendingAmountLabel(service, payer));
+				});
+				
+				jq_tooltip.css("display", "block");
+				jq_tooltip_arrow.css("display", "block");
+			}
+		});
+		
+		$(".tree_row_rollover").mouseleave(function() {
+			if (!year_playing) {
+				jq_tooltip.css("display", "none");
+				jq_tooltip_arrow.css("display", "none");
+			}
+		});	
+	}
+}
---
+++ js/chcf.js
@@ -0,0 +1,309 @@
+// Data
+var spending;
+var services_elements;
+var payers_elements;
+var years;
+var services_legend;
+var payers_legend;
+
+// Constants
+var TOTAL_SERVICES = "s1";
+var TOTAL_PAYERS = "p1";
+var TIMER_DURATION = 700;
+var TRANSITION_DURATION = 350;
+
+var LIGHTBOX_WIDTH = 960;
+var LIGHTBOX_HEIGHT = 571;
+var HEADER_HEIGHT = 44;
+var CONTROLS_HEIGHT = 35;
+var FOOTER_HEIGHT = 26;
+var CELL_BORDER_TB = 2;
+var CELL_BORDER_LR = 1;
+var CHART_WIDTH = LIGHTBOX_WIDTH - (CELL_BORDER_LR * 2);
+var CHART_HEIGHT = LIGHTBOX_HEIGHT - HEADER_HEIGHT - CONTROLS_HEIGHT - FOOTER_HEIGHT - (CELL_BORDER_TB * 2);
+var SLIDER_PADDING_L = 20;
+var SLIDER_PADDING_R = 12;
+var SLIDER_PADDING_T = 3;
+
+var ROW_LABEL_WIDTH = 300;
+var ROW_LABEL_PADDING_RT = 10;
+var TT_PADDING = 10;
+var TT_INDENT = 10;
+var TT_HEIGHT = 207;
+var TT_BORDER_WIDTH = 1;	
+var TT_ARROW_HEIGHT = 9;
+var TT_FOOTER_OFFSET = 5;
+
+// JQuery handles
+var jq_btn_play_year_i;
+var jq_year_label;
+var jq_tooltip;
+var jq_tooltip_arrow;
+var jq_caption_year;
+var jq_caption_spending;
+var jq_year_select;
+
+// Visualization
+var treemap;
+
+function formatSpending(val) {return (val / 1000).toFixed(1); }
+var formatPercent = d3.format("%");
+
+// State variables
+var current_year_index;
+
+// Playback control
+var year_playing = false;
+var year_timer = null;
+var init_play_timer = null;
+
+function initGlobal() {
+	// Init JQuery handles
+	jq_btn_play_year_i = $("#btn_play_year i");
+	jq_year_label = $("#year_label");
+	jq_tooltip = $("#tooltip");
+	jq_tooltip_arrow = $("#tooltip_arrow");
+	jq_caption_year = $("#caption_year");
+	jq_caption_spending = $("#caption_spending");
+	jq_year_select = $("#year_select");
+	
+	$.getJSON('data/spending.min.json', function(data) {
+		spending = data;
+
+		$.getJSON('data/schema.json', function(data) {
+			// Init data
+			services_elements = data.services_elements;
+			payers_elements = data.payers_elements;
+			years = data.years;
+			services_legend = data.services_legend;
+			payers_legend = data.payers_legend;
+			
+			// Init layout
+			initLayout();
+			
+			// Init year
+			current_year_index = 0;
+			buildYearSelect();
+			
+			jq_year_select.val(current_year_index);
+			jq_year_label.html(years[current_year_index]);
+			jq_caption_year.html(years[current_year_index]);
+			jq_caption_spending.html(formatSpending(getSpending(TOTAL_SERVICES, TOTAL_PAYERS)));
+			
+			// Init event handlers
+			initEventHandlers();
+			
+			// Init tooltip
+			initTooltip();
+			
+			// Init/render treemap
+			treemap = new TreemapVisualization("chart");
+			treemap.render();
+			
+			current_year_index = years.length - 1;
+			playYear();
+		});
+	})
+}
+
+function getSpending(service, payer) { return spending[years[current_year_index]][service][payer]; }
+
+function initEventHandlers() {	
+	jq_year_select.change(function() {
+		if (!year_playing) {
+			current_year_index = parseInt(this.value);
+			jq_year_label.html(years[current_year_index]);
+			jq_caption_year.html(years[current_year_index]);
+			jq_caption_spending.html(formatSpending(getSpending(TOTAL_SERVICES, TOTAL_PAYERS)));
+			jq_year_select.val(current_year_index);
+			treemap.update();
+		}
+	});
+	
+	$("#btn_play_year").click(function() {
+		$("#btn_play_year i").toggleClass('icon-play');
+		$("#btn_play_year i").toggleClass('icon-stop');	
+		if (!year_playing) {
+			playYear();
+		} else {
+			stopYear();
+		}
+	})
+	
+	$("#tt_link_notes")
+		.mouseenter(function() { $("#tt_notes").toggleClass("tt_visible"); })
+		.mouseleave(function() { $("#tt_notes").toggleClass("tt_visible"); });
+	
+	$("#btn_prev_year").click(function() { stopYear(); decrementYear(); })
+	$("#btn_next_year").click(function() { stopYear(); incrementYear(); })
+}
+
+function playYear() {
+	if (jq_btn_play_year_i.hasClass('icon-play')) jq_btn_play_year_i.removeClass('icon-play');
+	if (!jq_btn_play_year_i.hasClass('icon-stop')) jq_btn_play_year_i.addClass('icon-stop');	
+	
+	hideTooltips();
+	
+	year_playing = true;
+	year_timer = setInterval(function() {
+		incrementYear();
+	}, TIMER_DURATION);
+
+	if(current_year_index + 1 < years.length) current_year_index++;
+	else current_year_index = 0;
+	updateCurrentYear();
+}
+
+function stopYear() {
+	if (jq_btn_play_year_i.hasClass('icon-stop')) jq_btn_play_year_i.removeClass('icon-stop');
+	if (!jq_btn_play_year_i.hasClass('icon-play')) jq_btn_play_year_i.addClass('icon-play');	
+	
+	year_playing = false;
+	clearInterval(year_timer);
+}
+
+function incrementYear() {
+	if(current_year_index + 1 < years.length) {	 
+		current_year_index++;
+		updateCurrentYear();
+	}
+	else {
+		if (year_playing) stopYear();
+		else {
+			current_year_index = 0;
+			updateCurrentYear();
+		}
+	}
+}
+
+function decrementYear() {
+	if(current_year_index > 0) current_year_index--;
+	else current_year_index = years.length - 1;
+	updateCurrentYear();
+}
+
+function updateCurrentYear() {
+	jq_year_label.html(years[current_year_index]);
+	jq_caption_year.html(years[current_year_index]);
+	jq_caption_spending.html(formatSpending(getSpending(TOTAL_SERVICES, TOTAL_PAYERS)));
+	jq_year_select.val(current_year_index);
+	jq_year_select.trigger("change");
+	treemap.update();
+	
+	if (year_playing && current_year_index + 1 == years.length) {
+		if (jq_btn_play_year_i.hasClass('icon-stop')) jq_btn_play_year_i.removeClass('icon-stop');
+		if (!jq_btn_play_year_i.hasClass('icon-play')) jq_btn_play_year_i.addClass('icon-play');		
+	}
+}
+
+function hideTooltips() {
+	jq_tooltip.css("display", "none");
+	jq_tooltip_arrow.css("display", "none");	
+}
+
+var initTooltip = function() {
+	jq_tooltip.css("width", ROW_LABEL_WIDTH - (TT_PADDING * 2) - TT_INDENT + "px");
+	jq_tooltip.css("height", TT_HEIGHT - (TT_PADDING * 2) + "px");
+	jq_tooltip.css("padding", TT_PADDING + "px");
+	jq_tooltip.css("border-width", TT_BORDER_WIDTH + "px");
+	
+	jq_tooltip_arrow.css("left", ROW_LABEL_WIDTH - TT_INDENT + (TT_BORDER_WIDTH * 2) + "px");
+	
+	_.each(payers_elements, function(payer) {
+		var tt_label = "";
+		tt_label += "<div id='" + payer + "_tt' class='tt_row'>";
+		tt_label += 	"<div class='" + payer + " tt_legend'>&nbsp;</div>";
+		tt_label += 	"<div class='tt_legend_desc'>&nbsp;" + payers_legend[payer].title + "</div>";
+		tt_label += 	"<div class='tt_spending' id='tt_spending_" + payer + "'>&nbsp;</div>"
+		tt_label += "</div>";
+		jq_tooltip.append(tt_label);
+	});
+}
+
+function buildYearSelect() {	
+	// var year_select_values = _.clone(years).reverse();
+	_.each(years, function(year) {
+		jq_year_select.append("<option value='" + years.indexOf(year) + "'>" + year + "</option>")
+	})
+	
+	jq_year_select.selectToUISlider({
+		labels: 5,
+		labelSrc: "text",
+		tooltip: false
+	});
+	
+	$(".ui-slider").bind( "slidestop", function(event, ui) {
+		stopYear();
+		jq_year_select.trigger("change");
+	});
+}
+
+function initLayout() {
+	var jq_content = $("#content");
+	jq_content.css("width", LIGHTBOX_WIDTH + "px");
+	jq_content.css("height", LIGHTBOX_HEIGHT + "px");
+	
+	var jq_header = $("#header");
+	jq_header.css("width", LIGHTBOX_WIDTH + "px");
+	jq_header.css("height", HEADER_HEIGHT + "px");
+	jq_header.css("line-height", HEADER_HEIGHT + "px");
+	
+	var jq_controls = $("#controls");
+	jq_controls.css("width", LIGHTBOX_WIDTH + "px");
+	jq_controls.css("top", HEADER_HEIGHT + "px");
+	jq_controls.css("height", CONTROLS_HEIGHT + "px");
+	jq_controls.css("line-height", CONTROLS_HEIGHT + "px");
+	jq_controls.css("padding_left", ROW_LABEL_WIDTH + "px");
+	
+	var jq_chart = $("#chart");
+	jq_chart.css("top", HEADER_HEIGHT + CONTROLS_HEIGHT + "px");
+	jq_chart.css("width", LIGHTBOX_WIDTH + "px");
+	jq_chart.css("height", CHART_HEIGHT + "px");
+	
+	var jq_footer = $("#footer");
+	jq_footer.css("width", LIGHTBOX_WIDTH + "px");
+	jq_footer.css("height", FOOTER_HEIGHT + "px");
+	jq_footer.css("line-height", FOOTER_HEIGHT + "px");
+	jq_footer.css("top", LIGHTBOX_HEIGHT - FOOTER_HEIGHT + "px");
+	
+	var jq_footer_tooltip = $(".footer_tooltip");
+	jq_footer_tooltip.css("padding", TT_PADDING + "px");
+	jq_footer_tooltip.css("bottom", 0 - (CELL_BORDER_TB * 2) + "px");
+	jq_footer_tooltip.css("width", ROW_LABEL_WIDTH - (TT_PADDING * 2) - TT_INDENT + "px");
+	jq_footer_tooltip.css("height", CHART_HEIGHT - (TT_PADDING * 2) + "px");
+
+	
+	var jq_footnotes_link = $("#footnotes_link");
+	jq_footnotes_link.css("left", "0px");
+	jq_footnotes_link.css("width", ROW_LABEL_WIDTH - ROW_LABEL_PADDING_RT + "px");
+	jq_footnotes_link.css("padding-right", ROW_LABEL_PADDING_RT + "px");
+	jq_footnotes_link.css("height", FOOTER_HEIGHT + "px");
+	jq_footnotes_link.css("line-height", FOOTER_HEIGHT + "px");
+	
+	var jq_footer_legend = $("#footer_legend");
+	jq_footer_legend.css("left", ROW_LABEL_WIDTH + CELL_BORDER_LR + "px");
+	jq_footer_legend.css("width", CHART_WIDTH - ROW_LABEL_WIDTH - CELL_BORDER_LR + "px");
+	jq_footer_legend.css("height", FOOTER_HEIGHT + "px");
+	jq_footer_legend.css("line-height", FOOTER_HEIGHT + "px");
+	
+	_.each(payers_elements, function(payer) {
+		var footer_label = "";
+		footer_label += 	"<div class='" + payer + " footer_legend'>&nbsp;</div>";
+		footer_label += 	"<div class='footer_legend_desc'>&nbsp;" + payers_legend[payer].title + "</div>";
+		jq_footer_legend.append(footer_label);
+	});
+	
+	$(".footer_legend").css("top", (FOOTER_HEIGHT - $(".footer_legend").height()) / 2 + "px")
+	$(".footer_legend_desc").css("line-height", FOOTER_HEIGHT + "px");
+	
+	// Show the content once it's (mostly) been laid out
+	$("#content").css("display", "block");
+	
+	var jq_year_buttons = $("#year_buttons");
+	jq_year_buttons.css("left", ROW_LABEL_WIDTH + "px");
+	
+	var jq_year_slider = $("#year_slider");
+	jq_year_slider.css("width", LIGHTBOX_WIDTH - ROW_LABEL_WIDTH - jq_year_buttons.width() - SLIDER_PADDING_L - SLIDER_PADDING_R + "px");
+	jq_year_slider.css("right", SLIDER_PADDING_R + "px");
+	jq_year_slider.css("top", SLIDER_PADDING_T + "px");
+}
